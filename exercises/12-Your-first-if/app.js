0da05bff17b07d0ddab1752ce69fccb4a2f85360
dmedinam <danielmedinam@hotmail.com>
2021-03-21 21:06:28 +0000

    completado los primeros 11 ejercicios

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,15 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+let result 
+
+if (total>100)   
+    {result = "We still have a bit of driving left to go";}
+else if (total>50 && total<=100) 
+    {result = "We'll be there in 5 minutes";}
+else 
+    {result = "I'm parking. I'll see you right now";}
+
+
+console.log(result)
